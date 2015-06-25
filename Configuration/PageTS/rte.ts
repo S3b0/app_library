c95321ba3d6e4014bd564fcbb0bf66039f0143ec
S3b0 <Sebastian.Iffland@gmx.de>
2015-06-25 12:07:59 +0200

    RTE config

---
+++ Configuration/PageTS/rte.ts
@@ -0,0 +1,6 @@
+RTE.config.tx_applib_domain_model_app {
+	description.buttons.formatblock.removeItems = pre, address, article, aside, div, footer, header, nav, section
+	description.showButtons = formatblock, left, center, right, justifyfull, orderedlist, unorderedlist, bold, italic, strong, subscript, superscript, underline, insertcharacter, link, unlink, chMode, removeformat, undo, redo, outdent, indent, strikethrough, spellcheck, textcolor, bgcolor
+	features.showButtons = bold, italic, subscript, superscript, underline, orderedlist, unorderedlist, insertcharacter, link, chMode, removeformat, undo, redo, strikethrough, spellcheck, textcolor, bgcolor
+	system_requirements.showButtons < RTE.config.tx_applib_domain_model_app.features.showButtons
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -33,3 +33,5 @@ if (!defined('TYPO3_MODE')) {
 	'Faq',
 	[ 'Faq' => 'list' ]
 );
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/rte.ts">');
