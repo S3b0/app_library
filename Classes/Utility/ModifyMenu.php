328d2c8013160f756bed0957f419f6e513166ce6
S3b0 <Sebastian.Iffland@gmx.de>
2015-07-07 13:31:55 +0200

    Update breadcrumb generator

--- Classes/Utility/ModifyMenu.php
+++ Classes/Utility/ModifyMenu.php
@@ -14,32 +14,32 @@
 	class ModifyMenu extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin {
 
 		/**
-		 * @param array $item
-		 * @param array $pObj
+		 * @param array $menuArray
+		 * @param array $configuration
 		 *
 		 * @return array
 		 */
-		public function bradcrumb($item, $pObj) {
+		public function breadcrumb($menuArray, $configuration) {
 			/** @var \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer cObj */
 			$this->cObj = Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\ContentObject\\ContentObjectRenderer');
 			$rootLine = $GLOBALS['TSFE']->rootLine;
 			array_walk($rootLine, 'self::getSingleArrayValue', 'uid');
-			$this->parseRootLineMenu($item, $pObj);
+			$this->parseRootLineMenu($menuArray, $configuration);
 
-			return $item;
+			return $menuArray;
 		}
 
 		/**
-		 * @param array &$item
-		 * @param array $pObj
+		 * @param array &$menuArray
+		 * @param array $configuration
 		 *
 		 * @return void
 		 */
-		public function parseRootLineMenu(&$item = [ ], $pObj = [ ]) {
+		public function parseRootLineMenu(&$menuArray = [ ], $configuration = [ ]) {
 			$GPvar = Utility\GeneralUtility::_GPmerged('tx_applibrary_list');
 			$localizations = (bool) $GLOBALS['TYPO3_DB']->exec_SELECTcountRows ('*', 'sys_language', '1=1' . $this->cObj->enableFields('sys_language'));
 			if (Utility\MathUtility::canBeInterpretedAsInteger($GPvar['app']) && $GPvar['app']) {
-				$last = array_pop($item); // Set array pointer to last object
+				$last = array_pop($menuArray); // Set array pointer to last object
 				$record = $this->pi_getRecord('tx_applib_domain_model_app', Utility\MathUtility::convertToPositiveInteger($GPvar['app']));
 				if ( $GLOBALS['TSFE']->sys_language_content && $GLOBALS['TSFE']->sys_language_contentOL !== 'hideNonTranslated' ) {
 					$record = $GLOBALS['TSFE']->sys_page->getRecordOverlay('tx_applib_domain_model_app', $record, $GLOBALS['TSFE']->sys_language_content, $GLOBALS['TSFE']->sys_language_contentOL);
@@ -48,7 +48,7 @@
 					return NULL;
 				}
 				$last['ITEM_STATE'] = 'ACTIFSUB';
-				$item[] = $last;
+				$menuArray[] = $last;
 				$typoLink_Conf['parameter'] = &$GLOBALS['TSFE']->id;
 				$typoLink_Conf['useCacheHash'] = 1;
 				$typoLink_Conf['additionalParams'] = '&tx_applibrary_list[app]=' . Utility\MathUtility::convertToPositiveInteger($GPvar['app']); // Add piVars
@@ -56,8 +56,8 @@
 				if ( $localizations ) {
 					$typoLink_Conf['additionalParams'] .= '&L=' . $GLOBALS['TSFE']->sys_language_content;
 				}
-				$item[] = array(
-					'title' => $record['name'],
+				$menuArray[] = array(
+					'title' => $record['title'],
 					'_OVERRIDE_HREF' => $this->cObj->typoLink_URL($typoLink_Conf),
 					'ITEM_STATE' => 'CUR',
 					'_SAFE' => TRUE
